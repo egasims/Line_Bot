22be8fccf964976f1db57947e10acee6a31076b6
F07446 <nickuo@egas.com.tw>
2024-11-21 15:47:49 +0800

    update img_url_edit 20241121

--- Module/messageModule.py
+++ Module/messageModule.py
@@ -35,12 +35,9 @@ mesDic = dict({"交通資訊": return_flex("交通資訊", trainsitPage()),
                "廠辦G7(新)": return_img("https://i.imgur.com/21hj0fR.jpg", "https://i.imgur.com/21hj0fR.jpg"),
                "廠辦G8(新)": return_img("https://i.imgur.com/gd7XTRE.jpg", "https://i.imgur.com/gd7XTRE.jpg"),
                "長興線": return_flex("長興線", multilePage2("https://i.imgur.com/ZwVQ3F7.jpg", "每日行駛",
-                                                            "https://i.imgur.com/r0YLJWE.jpg
-", "工作日行駛")),
+                                                            "https://i.imgur.com/r0YLJWE.jpg", "工作日行駛")),
                "每日行駛": return_img("https://i.imgur.com/ZwVQ3F7.jpg", "https://i.imgur.com/ZwVQ3F7.jpg"),
-               "工作日行駛": return_img("https://i.imgur.com/r0YLJWE.jpg
-", "https://i.imgur.com/r0YLJWE.jpg
-"),
+               "工作日行駛": return_img("https://i.imgur.com/r0YLJWE.jpg", "https://i.imgur.com/r0YLJWE.jpg"),
                "A15線": return_flex("A15線", multilePage2("https://i.imgur.com/HDwDhvC.jpg", "A15線去程",
                                                           "https://i.imgur.com/byBAbBm.jpg", "A15線回程")),
                "A15線去程": return_img("https://i.imgur.com/HDwDhvC.jpg", "https://i.imgur.com/HDwDhvC.jpg"),
